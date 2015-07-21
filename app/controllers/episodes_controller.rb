36d2a7b7a0fac07e06f8b6d124daa793a28b2cbe
Magnus Skog <magnus.m.skog@gmail.com>
2015-07-21 13:11:50 +0200

    Ok this worked

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -13,8 +13,7 @@ class EpisodesController < ApplicationController
 
   def download
     @view = Domain::Episode.new(Episode.find(params[:id]))
-    # name = "#{@view.name}.S#{@view.season.to_s.rjust(2,'0')}E#{@view.episode.to_s.rjust(2,'0')}.torrent".gsub(' ', '.')
-    # send_data Faraday.get(@view.best_release.url).body, filename: name, type: 'application/x-bittorrent'
-    redirect_to @view.best_release.url
+    name = "#{@view.name}.S#{@view.season.to_s.rjust(2,'0')}E#{@view.episode.to_s.rjust(2,'0')}.torrent".gsub(' ', '.')
+    send_data Faraday.get(@view.best_release.url).body, filename: name, type: 'application/x-bittorrent'
   end
 end
