2bcbefa61e8f56afe39fb42091c9cab94fd9d8d3
Magnus Skog <magnus.m.skog@gmail.com>
2015-07-24 14:41:19 +0200

    Sort by desc

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -2,7 +2,7 @@ class EpisodesController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_USERNAME'], password: ENV['HTTP_PASSWORD'], except: :download
 
   def index
-    @view = Episode.eager_load(:releases).where("episodes.published_at < ?", ENV['DELAY_HOURS'].to_i.hours.ago).order(id: :asc).limit(100).map do |episode|
+    @view = Episode.eager_load(:releases).where("episodes.published_at < ?", ENV['DELAY_HOURS'].to_i.hours.ago).order(id: :desc).limit(100).map do |episode|
       Domain::Episode.new(episode)
     end
 
