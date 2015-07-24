33f5cb4bfd129370087947902452b471a9dace77
Magnus Skog <magnus.m.skog@gmail.com>
2015-07-24 14:01:56 +0200

    Error handling

--- config/clock.rb
+++ config/clock.rb
@@ -3,6 +3,10 @@ require './config/boot'
 require './config/environment'
 
 module Clockwork
+  error_handler do |error|
+    Rollbar.error(error)
+  end
+
   every(30.minutes, 'Downloaded updates from BTN Feed') do
     time = Release.maximum(:published_at) || Time.parse('1970-01-01')
     Services::FetchAndPersistFeedEntries.new(ENV['BTN_FEED_URL'], time).perform
