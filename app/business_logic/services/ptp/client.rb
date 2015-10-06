70fef3fbbabc6ba5544fd25729c9189118b10635
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-06 20:45:28 +0200

    Less smell

--- app/business_logic/services/ptp/client.rb
+++ app/business_logic/services/ptp/client.rb
@@ -5,17 +5,21 @@ module Services
       LOGIN_URL = "ajax.php?action=login"
 
       def initialize
-        @connection = Faraday.new(:url => API_URL) do |builder|
-          builder.use :cookie_jar
-          builder.request  :url_encoded
-          builder.response :json
-          builder.adapter Faraday.default_adapter
-        end
+        @connection = self.class.build_client
         login
         super @connection
       end
 
       private
+
+      def self.build_client
+        Faraday.new(:url => API_URL) do |builder|
+          builder.use :cookie_jar
+          builder.request  :url_encoded
+          builder.response :json
+          builder.adapter Faraday.default_adapter
+        end
+      end
 
       def login
         # TODO Check for non-200?
