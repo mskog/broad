99efd83ae97a230634e2477d3992bfa2dfd03d00
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-07 21:10:28 +0200

    No more smelling

--- app/business_logic/services/fetch_and_persist_feed_entries.rb
+++ app/business_logic/services/fetch_and_persist_feed_entries.rb
@@ -8,23 +8,18 @@ module Services
     def perform
       feed.published_since(@published_since).each do |entry|
         next unless entry.name.present?
-        episode = Episode.find_or_create_by(name: entry.name, season: entry.season, episode: entry.episode, year: entry.year) do |episode|
-          episode.published_at = entry.published_at
-        end
-        release_attributes = {
-          title: entry.title,
-          url: entry.url,
-          file_type: entry.file_type,
-          file_encoding: entry.file_encoding,
-          source: entry.source,
-          resolution: entry.resolution,
-          published_at: entry.published_at
-        }
-        episode.releases.find_or_create_by(release_attributes)
+        episode = self.class.build_episode(entry)
+        episode.releases.find_or_create_by(entry.to_h.except(:name, :episode, :year, :season))
       end
     end
 
     private
+
+    def self.build_episode(entry)
+      @episode = Episode.find_or_create_by(entry.to_h.slice(:name, :episode, :year, :season)) do |episode|
+        episode.published_at = entry.published_at
+      end      
+    end
 
     def feed
       @feed ||= Domain::BTN::Feed.new(@feed_url)
