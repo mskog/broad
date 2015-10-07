204d1bf6e9309b769271eec7684e7c9cb681fe3d
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-07 20:45:34 +0200

    Less smell

--- spec/business_logic/domain/ptp/movie_spec.rb
+++ spec/business_logic/domain/ptp/movie_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Domain::PTP::Movie do
-  subject{described_class.new(PTPFixturesHelper.build_stubbed(movie_fixture))}
+  Given(:movie){PTPFixturesHelper.build_stubbed(movie_fixture)}
+  subject{described_class.new(movie)}
 
   describe "#best_release" do
     When(:result){subject.best_release}
@@ -24,12 +25,43 @@ describe Domain::PTP::Movie do
 
     context "with a movie with a 3d release" do
       Given(:movie_fixture){'up'}
-      Then{expect(result.ptp_movie_id).to eq 98064}      
+      Then{expect(result.ptp_movie_id).to eq 98064}
     end
 
     context "with a movie with a remux" do
       Given(:movie_fixture){'lincoln_lawyer'}
-      Then{expect(result.ptp_movie_id).to eq 298502}            
+      Then{expect(result.ptp_movie_id).to eq 298502}
+    end
+  end
+
+  describe "#set_attributes" do
+    Given(:movie){build_stubbed :movie}
+    Given(:ptp_movie){OpenStruct.new(title: 'The Matrix', imdb_id: 12345)}
+    When{subject.set_attributes(ptp_movie)}
+    Then{expect(subject.title).to eq ptp_movie.title}
+    And{expect(subject.imdb_id).to eq "tt#{ptp_movie.imdb_id}"}
+  end
+
+  describe "#fetch_new_releases" do
+    Given do
+      stub_request(:post, "https://tls.passthepopcorn.me/ajax.php?action=login").
+               with(:body => {"passkey"=>ENV['PTP_PASSKEY'], "password"=>ENV['PTP_PASSWORD'], "username"=>ENV['PTP_USERNAME']},
+                    :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Faraday v0.9.1'}).
+               to_return(:status => 200, :body => "", :headers => {})
+    end
+
+    Given(:movie){build_stubbed :movie}
+    Given(:ptp_api){Services::PTP::Api.new}
+    Given(:ptp_movie){ptp_api.search(movie.imdb_id).movie}
+    When{subject.fetch_new_releases(ptp_movie)}
+
+    context "when the movie currently has no releaes" do
+      Given do
+        stub_request(:get, "https://tls.passthepopcorn.me/torrents.php?json=noredirect&searchstr=#{movie.imdb_id}")
+            .to_return(:status => 200, :body => File.read('spec/fixtures/ptp/brotherhood_of_war.json'))
+      end
+
+      Then{expect(movie.movie_releases.size).to eq 7}
     end
   end
 end
