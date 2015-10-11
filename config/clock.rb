4b805a7c3cfabb9f5a560826cc12e63d424f7d68
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-11 19:19:43 +0200

    Use thread

--- config/clock.rb
+++ config/clock.rb
@@ -7,14 +7,15 @@ module Clockwork
     Rollbar.error(error)
   end
 
-  every(1.hour, 'Downloaded updates from BTN Feed', :at => '**:30') do
+  every(1.hour, 'Downloaded updates from BTN Feed', :at => '**:30', thread: true) do
     time = EpisodeRelease.maximum(:published_at) || Time.parse('1970-01-01')
     Services::FetchAndPersistFeedEntries.new(ENV['BTN_FEED_URL'], time).perform
   end
 
-  every(12.hour, 'Download new releases for Overwatch movies', :at => ["12:00", "18:00"]) do
+  every(12.hour, 'Download new releases for Overwatch movies', :at => ["12:00", "18:00"], thread: true) do
     Movie.on_overwatch.each do |movie|
       Services::FetchNewMovieReleases.new(movie).perform
+      sleep 1
     end
   end
 end
