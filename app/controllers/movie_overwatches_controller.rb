67922874b2b4e1bf92837aab7f49a0f849121097
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-11 19:35:17 +0200

    Creating overwatches

--- app/controllers/movie_overwatches_controller.rb
+++ app/controllers/movie_overwatches_controller.rb
@@ -5,8 +5,8 @@ class MovieOverwatchesController < ApplicationController
 
   def create
     imdb = Services::Imdb.from_data(create_params[:query])
-    movie = Movie.new(imdb_id: imdb.id, overwatch: true)
-    Services::FetchNewMovieReleases.new(movie).perform
+    movie = Movie.create(imdb_id: imdb.id, overwatch: true)
+    FetchNewMovieReleasesJob.perform_later movie
     redirect_to movie_overwatches_path
   end
 
