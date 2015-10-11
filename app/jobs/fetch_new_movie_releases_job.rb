87ec37c0c1a871e261706a7e1174efe82fafa7fe
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-11 19:30:51 +0200

    Job, request specs

---
+++ app/jobs/fetch_new_movie_releases_job.rb
@@ -0,0 +1,15 @@
+class FetchNewMovieReleasesJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(movie)
+    ActiveRecord::Base.connection_pool.with_connection do
+      fetch_releases(movie)
+    end
+  end
+
+  private
+
+  def fetch_releases(movie)
+    Services::FetchNewMovieReleases.new(movie).perform
+  end
+end
