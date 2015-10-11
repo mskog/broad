3d7d6da8014dfacffebaa4dc13923e2cdcbd76f2
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-11 19:00:20 +0200

    Clock changed to not grab on every deploy

--- config/clock.rb
+++ config/clock.rb
@@ -7,7 +7,7 @@ module Clockwork
     Rollbar.error(error)
   end
 
-  every(30.minutes, 'Downloaded updates from BTN Feed') do
+  every(1.hour, 'Downloaded updates from BTN Feed', :at => '**:30') do
     time = EpisodeRelease.maximum(:published_at) || Time.parse('1970-01-01')
     Services::FetchAndPersistFeedEntries.new(ENV['BTN_FEED_URL'], time).perform
   end
