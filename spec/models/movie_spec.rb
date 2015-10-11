566a165462c2f576244c5d1b950ae14bf8089a37
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-11 18:37:08 +0200

    Added scope for overwatch movies

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,10 +3,17 @@ require 'spec_helper'
 describe Movie do
   it{is_expected.to have_many(:releases).class_name(MovieRelease)}
 
-  describe ".for_download" do
+  describe ".downloadable" do
     Given!(:movie){create :movie, overwatch: false}
     Given!(:movie_overwatch){create :movie, overwatch: true}
-    When(:result){described_class.for_download}
+    When(:result){described_class.downloadable}
     Then{expect(result).to contain_exactly(movie)}
+  end
+
+  describe ".on_overwatch" do
+    Given!(:movie){create :movie, overwatch: false}
+    Given!(:movie_overwatch){create :movie, overwatch: true}
+    When(:result){described_class.on_overwatch}
+    Then{expect(result).to contain_exactly(movie_overwatch)}
   end
 end
