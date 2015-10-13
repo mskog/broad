e038d790caa5b64ada7924a7792195a18029dc49
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-13 21:15:01 +0200

    Release rules for overwatch

---
+++ app/business_logic/domain/ptp/release_rules/overwatch.rb
@@ -0,0 +1,16 @@
+module Domain
+  module PTP
+    module ReleaseRules
+      # TODO specs
+      class Overwatch
+        def initialize(release)
+          @release = release
+        end
+
+        def acceptable?
+          !@release.version_attributes.include?("3d") && @release.source == 'blu-ray'
+        end
+      end
+    end
+  end
+end
