938d49a2a0bc14d0d76173eb23d004842111d782
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-15 14:25:14 +0200

    SSL for mr murray

--- app/decorators/movie_search_result_decorator.rb
+++ app/decorators/movie_search_result_decorator.rb
@@ -6,7 +6,7 @@ class MovieSearchResultDecorator < Draper::Decorator
     if movie_poster != 'N/A'
       "https://thumbs.picyo.me/200x0/filters:quality(50)/#{movie_poster}"
     else
-      "http://www.fillmurray.com/300/444"
+      "https://www.fillmurray.com/300/444"
     end
   end
 
