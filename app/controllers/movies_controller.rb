5929c3c74b77300f5b7c88bc8144096f84258cec
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-17 17:51:24 +0200

    Ghetto real time is ghetto

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,9 @@
 class MoviesController < ApplicationController
+  def show
+    @view = MovieDecorator.decorate Domain::PTP::Movie.new(Movie.find(params[:id]))
+    render 'movies/show', layout: false
+  end
+
   def destroy
     @view = Movie.find(params[:id])
     @view.destroy if @view.deletable?
