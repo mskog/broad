fc16f386c34e6d1ec4d611857b6574842b5d2210
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-19 21:26:02 +0200

    Update all waitlist movies daily with new information from omdb

--- config/clock.rb
+++ config/clock.rb
@@ -15,4 +15,9 @@ module Clockwork
   every(12.hour, 'Download new releases for waitlist movies', :at => ["08:00", "22:00"], thread: true) do
     WaitlistMoviesCheckJob.perform_later
   end
+
+  every(1.day, 'Update all waitlist movie details', :at => ["01:00"], thread: true) do
+    UpdateAllMovieDetailsJob.perform_later
+  end
+
 end
