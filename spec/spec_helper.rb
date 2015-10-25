6b07094119019a1174ff1e2fa29835e9babd6351
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-25 16:34:57 +0100

    No coverage for spec dir plz

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,9 @@ if ENV['CIRCLE_ARTIFACTS']
   require 'simplecov'
   dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
   SimpleCov.coverage_dir(dir)
-  SimpleCov.start
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
 end
 
 
