51d3bbbfef683a215cf2e00bed7773b94392dc28
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-25 14:48:38 +0100

    Refetch episode details when necessary

--- app/jobs/fetch_episode_details_job.rb
+++ app/jobs/fetch_episode_details_job.rb
@@ -4,6 +4,7 @@ class FetchEpisodeDetailsJob < ActiveJob::Base
   def perform(episode)
     ActiveRecord::Base.connection_pool.with_connection do
       fetch_details(episode)
+      refetch_if_necessary(episode)
     end
     sleep 1 unless Rails.env.test?
   end
@@ -14,5 +15,12 @@ class FetchEpisodeDetailsJob < ActiveJob::Base
     data = Tmdb::Episode.detail(episode.tv_show.tmdb_details['id'], episode.season, episode.episode)
     return if data['status_code'].present?
     episode.update tmdb_details: data
+  end
+
+  def refetch_if_necessary(episode)
+    tmdb_details = episode.tmdb_details
+    return if tmdb_details.present? && (tmdb_details['still_path'].present? && tmdb_details['overview'].present?)
+    return if episode.created_at < Date.today-1.week
+    self.class.set(wait: 1.hour).perform_later episode
   end
 end
