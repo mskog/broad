3425e37ddca7c5e6d33967106bd1898bd9dfc72f
Magnus Skog <magnus.m.skog@gmail.com>
2015-10-26 21:03:03 +0100

    Very nice limited display

--- app/decorators/dashboard_decorator.rb
+++ app/decorators/dashboard_decorator.rb
@@ -1,5 +1,19 @@
 class DashboardDecorator < Draper::Decorator
+  MOVIES_WAITLIST_LIMIT = 10
+
   def movies_waitlist
-    MovieDecorator.decorate_collection(object.movies_waitlist)
+    @movies_waitlist ||= MovieDecorator.decorate_collection(object.movies_waitlist)
+  end
+
+  def movies_waitlist_more?
+    movies_waitlist.size > MOVIES_WAITLIST_LIMIT
+  end
+
+  def movies_waitlist_more_nbr
+    movies_waitlist_more? ? movies_waitlist.size - MOVIES_WAITLIST_LIMIT : 0
+  end
+
+  def movies_waitlist_limit
+    MOVIES_WAITLIST_LIMIT
   end
 end
