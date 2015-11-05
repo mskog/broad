11c94d643759c57565374efebd6a2708cbef4bb5
Magnus Skog <magnus.m.skog@gmail.com>
2015-11-05 12:49:21 +0100

    Added first api methods

---
+++ app/controllers/api/v1/movie_waitlists_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::MovieWaitlistsController < Api::ApiController
+  def create
+    movie = Movie.find_or_create_by(imdb_id: create_params[:imdb_id], waitlist: true)
+    CheckWaitlistMovieJob.perform_later movie
+    head 200
+  end
+
+  private
+
+  def create_params
+    params.require(:imdb_id)
+    params.permit(:imdb_id)
+  end
+end
