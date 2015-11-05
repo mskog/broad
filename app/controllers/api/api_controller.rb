10a56b648c0bb314be65c50dddbdba34192bf77e
Magnus Skog <magnus.m.skog@gmail.com>
2015-11-05 12:49:38 +0100

    Dang

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,4 @@
 class Api::ApiController < ApplicationController
   protect_from_forgery with: :null_session
-  skip_before_action :verify_authenticity_tokend
+  skip_before_action :verify_authenticity_token
 end
