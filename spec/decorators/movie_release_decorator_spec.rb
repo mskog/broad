e46c0aed80f3de5cb5f34194abfeef121daf301e
Magnus Skog <magnus.m.skog@gmail.com>
2016-02-11 19:44:39 +0100

    Extraction of killer releases rule class

--- spec/decorators/movie_release_decorator_spec.rb
+++ spec/decorators/movie_release_decorator_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe MovieReleaseDecorator, :nodb do
   subject{described_class.new(movie_release)}
 
-  Given(:movie_release){build_stubbed :movie_release}
+  Given(:movie_release){build_stubbed :movie_release, version_attributes: ['remux']}
 
   describe "#release_name" do
     Then{expect(subject.release_name).to eq 'Jurassic.World.2015.Bdrip.X264 Sparks'}
