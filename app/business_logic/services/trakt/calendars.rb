4add12e4e0d75e1a750e38a60f756441075e8539
Magnus Skog <magnus.m.skog@gmail.com>
2016-02-23 19:55:43 +0100

    Improved structure

--- app/business_logic/services/trakt/calendars.rb
+++ app/business_logic/services/trakt/calendars.rb
@@ -7,7 +7,9 @@ module Services
       end
 
       def shows(from_date: Date.today, days: 7)
-        request("calendars/my/shows/#{from_date}/#{days}").body
+        request("calendars/my/shows/#{from_date}/#{days}").body.map do |result|
+          Result.new(result)
+        end
       end
 
       private
@@ -16,6 +18,44 @@ module Services
         @client.get(route) do |request|
           request.headers['authorization'] = "Bearer #{@token}"
         end
+      end
+
+      class Ids
+        include Virtus.model
+
+        attribute :imdb
+        attribute :tmdb
+        attribute :trakt
+        attribute :tvdb
+        attribute :tvrage
+        attribute :slug
+      end
+
+      class Episode
+        include Virtus.model
+
+        attribute :ids, Ids
+
+        attribute :number, Integer
+        attribute :season, Integer
+        attribute :title, String
+      end
+
+      class Show
+        include Virtus.model
+
+        attribute :ids, Ids
+
+        attribute :title, String
+        attribute :year, Integer
+      end
+
+      class Result
+        include Virtus.model
+
+        attribute :episode, Episode
+        attribute :show, Show
+        attribute :first_aired, DateTime
       end
     end
   end
