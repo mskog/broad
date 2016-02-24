fc18c54cd76792d0dd022c508c863df501365f5f
Magnus Skog <magnus.m.skog@gmail.com>
2016-02-24 20:28:08 +0100

    Fixed spec lookup failure

--- app/business_logic/services/trakt/data/movie_with_details.rb
+++ app/business_logic/services/trakt/data/movie_with_details.rb
@@ -1,7 +1,7 @@
 module Services
   module Trakt
     module Data
-      class MovieWithDetails < Movie
+      class MovieWithDetails < Services::Trakt::Data::Movie
         attribute :images, Images
 
         attribute :overview
