6c4882cc140bac2192c8575f46553300a97de8cb
Magnus Skog <magnus.m.skog@gmail.com>
2016-03-02 13:08:20 +0100

    Update all tv show details scheduled

---
+++ app/jobs/update_all_tv_show_details_job.rb
@@ -0,0 +1,10 @@
+class UpdateAllTvShowDetailsJob < ActiveJob::Base
+  queue_as :default
+
+  def perform
+    TvShow.find_each do |tv_show|
+      FetchTvShowDetailsTmdbJob.perform_later tv_show
+      FetchTvShowDetailsTraktJob.perform_later tv_show
+    end
+  end
+end
