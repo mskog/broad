780d004315082ecfb113172d6705fe8978e6e36a
Magnus Skog <magnus.m.skog@gmail.com>
2016-03-03 19:41:10 +0100

    Paginatable converns

--- app/interface/view_objects/support/paginatable.rb
+++ app/interface/view_objects/support/paginatable.rb
@@ -2,7 +2,7 @@ module ViewObjects
   module Support
     module Paginatable
 
-      PER_PAGE = 15
+      PER_PAGE = 20
 
       def paginate(page:, per_page: PER_PAGE)
         __setobj__ __getobj__.page(page).per(per_page)
