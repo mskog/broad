5a7273a5a07e1933584faaa6cf324576e574d3ea
Magnus Skog <magnus.m.skog@gmail.com>
2016-03-10 19:33:19 +0100

    Download buttons

--- app/react/components/movie_search/result.jsx
+++ app/react/components/movie_search/result.jsx
@@ -4,6 +4,7 @@ import Title from './title';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import Panel from 'react-bootstrap/lib/Panel';
+import Button from 'react-bootstrap/lib/Button';
 
 class Result extends React.Component {
   render() {
@@ -15,7 +16,27 @@ class Result extends React.Component {
                 <PosterImage src={this.props.poster} />
               </Col>
               <Col md={10}>
-                <Title title={this.props.title} year={this.props.year} />
+                <Row>
+                  <Col md={12}>
+                    <Title title={this.props.title} year={this.props.year} />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={12}>
+                    <form method='post' action={`/movie_downloads?query=${this.props.imdb_id}`}>
+                      <Button type='submit' bsStyle='success' bsSize="small">
+                        Download
+                      </Button>
+                    </form>
+
+                    <form className="m-t-sm" method='post' action={`/movie_waitlists?query=${this.props.imdb_id}`}>
+                      <Button type='submit' bsStyle='success' bsSize="small">
+                        Add to Waitlist
+                      </Button>
+                    </form>
+                  </Col>
+                </Row>
+
               </Col>
             </Row>
           </Panel>
