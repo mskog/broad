4306dbcc30752347605af7633e2d194d467132e8
Magnus Skog <magnus.m.skog@gmail.com>
2016-03-12 14:43:05 +0100

    Nice rt icons

--- app/react/components/movie_search/details.jsx
+++ app/react/components/movie_search/details.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 
+import RtIcon from '../rt_icon';
+
 class Details extends React.Component {
   render() {
     return(
       <Row>
         <Col md={6}>
-          RT: {this.props.tomato_meter}%
+          <RtIcon tomato_meter={this.props.tomato_meter} />
         </Col>
-
       </Row>
     );
   }
