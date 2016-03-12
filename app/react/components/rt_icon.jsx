be432cd2da1536842c389ffc7d2b6b338970f3a2
Magnus Skog <magnus.m.skog@gmail.com>
2016-03-12 14:43:05 +0100

    Nice rt icons

---
+++ app/react/components/rt_icon.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+class RtIcon extends React.Component {
+  icon() {
+    var imageUrl = "";
+    if (this.props.tomato_meter >= 60){
+      imageUrl = '/images/rt_fresh.png'
+    }else{
+      imageUrl = '/images/rt_rotten.png'
+    }
+
+    return <img width="50" height="50" className='media-object' src={imageUrl} />;
+  }
+
+  render() {
+    return(
+      <div>
+        <div className='pull-left'>
+          {this.icon()}
+        </div>
+        <div className='vertical-center p-l-xs pull-left'>
+          <h4>
+            {this.props.tomato_meter}%
+          </h4>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RtIcon;
