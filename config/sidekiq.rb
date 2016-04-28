1595a6891e14983bc02572f107ac6480cd64301e
Magnus Skog <magnus.m.skog@gmail.com>
2016-04-28 14:04:13 +0200

    Didnt work. Screw it

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,8 +1,3 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL']}
 end
-
-Sidekiq.default_worker_options = {
-  unique: :until_executing,
-  unique_args: ->(args) { args.first.except('job_id') }
-}
