af67f9ecfd826ccc0d5a40c5506668b545e54329
Magnus Skog <magnus.m.skog@gmail.com>
2016-04-30 11:46:48 +0200

    Do not retry btn is down errors

--- app/jobs/fetch_new_feed_entries_job.rb
+++ app/jobs/fetch_new_feed_entries_job.rb
@@ -1,6 +1,10 @@
 class FetchNewFeedEntriesJob < ActiveJob::Base
   queue_as :default
 
+  rescue_from(Services::BTN::Feed::BTNIsProbablyDownError) do |exception|
+    raise exception
+  end
+
   def perform
     time = EpisodeRelease.maximum(:published_at) || Time.parse('1970-01-01')
     Services::FetchAndPersistFeedEntries.new(ENV['BTN_FEED_URL'], time).perform
