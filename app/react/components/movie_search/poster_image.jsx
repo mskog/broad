65aff0d8d7f158aae4752d2fcb5d0b3fa4e3c088
Magnus Skog <magnus.m.skog@gmail.com>
2016-10-09 13:56:40 +0200

    Lazy loaded images for movie search

--- app/react/components/movie_search/poster_image.jsx
+++ app/react/components/movie_search/poster_image.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import LazyLoad from 'react-lazy-load';
 
 // TODO Placeholder images
 class PosterImage extends React.Component {
   render() {
     return(
-      <img className='img-responsive' src={this.props.src} />
+      <LazyLoad offsetVertical={200}>
+        <img className='img-responsive' src={this.props.src} />
+      </LazyLoad>
     );
   }
 }
