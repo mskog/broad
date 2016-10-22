f6b3c8894e45094277293a5a2c909e85f7457c29
Magnus Skog <magnus.m.skog@gmail.com>
2016-10-22 11:10:09 +0200

    Reuse of posterimage component

--- app/react/components/movie_recommendations/recommendation.jsx
+++ app/react/components/movie_recommendations/recommendation.jsx
@@ -4,23 +4,19 @@ import Col from 'react-bootstrap/lib/Col';
 import ImageLoader from 'react-imageloader';
 import FontAwesome from 'react-fontawesome';
 import {imagePath} from 'rwr-view-helpers';
+import PosterImage from '../movie_search/poster_image';
 
 class Recommendation extends React.Component {
-  preloader() {
-    return <img className='img-responsive' src={imagePath('placeholder_movie_poster.jpg')} />;
-  }
-
   render() {
     var posterSrc = `/movie_posters/${this.props.tmdb_id}`;
-    var imgProps = {className: 'img-responsive'}
 
     return(
         <li className="col-md-3">
-          <ImageLoader imgProps={imgProps} src={posterSrc} preloader={this.preloader} />
-          <div className="m-t">
-            Download
-            Hide
-          </div>
+        <PosterImage src={posterSrc} />
+        <div className="m-t">
+          Download
+          Hide
+        </div>
         </li>
     );
   }
--- app/react/components/movie_search/poster_image.jsx
+++ app/react/components/movie_search/poster_image.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import LazyLoad from 'react-lazy-load';
 import ImageLoader from 'react-imageloader';
 import FontAwesome from 'react-fontawesome';
+import {imagePath} from 'rwr-view-helpers';
 
+// TODO Move this up a level
 class PosterImage extends React.Component {
   preloader() {
-    return <FontAwesome spin size='2x' name='circle-o-notch'/>;
+    return <img className='img-responsive' src={imagePath('placeholder_movie_poster.jpg')} />;
   }
 
   render() {
