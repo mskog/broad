edb81b7dfda1f55642645649c21fab0698d4eca4
Magnus Skog <magnus.m.skog@gmail.com>
2016-10-23 17:27:32 +0200

    Movie recommendations shouldnt include movies we have downloaded or have on waitlist

--- spec/interface/view_objects/movie_recommendations_spec.rb
+++ spec/interface/view_objects/movie_recommendations_spec.rb
@@ -5,8 +5,9 @@ describe ViewObjects::MovieRecommendations do
 
   describe "Enumeration" do
     Given!(:movie_recommendations){create_list :movie_recommendation, 2}
+    Given{create :movie, imdb_id: movie_recommendations.last.imdb_id}
     When(:result){subject.to_a}
-    Then{expect(result.count).to eq 2}
+    Then{expect(result.count).to eq 1}
     And{expect(result.first.title).to eq movie_recommendations.first.title}
   end
 end
