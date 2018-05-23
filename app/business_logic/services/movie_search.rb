797139acf0f23dd60342f2588f88d6b5ba073f4c
Magnus Skog <magnus.m.skog@gmail.com>
2018-05-23 20:56:49 +0200

    Extracted object from search

--- app/business_logic/services/movie_search.rb
+++ app/business_logic/services/movie_search.rb
@@ -28,55 +28,6 @@ module Services
       end
     end
 
-    class Query
-      def initialize(query)
-        @query = build_query(query)
-      end
-
-      def imdb_id
-        @query.try(:imdb_id)
-      end
-
-      def query
-        @query.query
-      end
-
-      private
-
-      def build_query(query)
-        query_services.find do |service|
-          service.matches? query
-        end.from_data(query)
-      end
-
-      def query_services
-        [
-          Services::Imdb,
-          Services::RottenTomatoes,
-          Services::Metacritic,
-          Anything,
-        ]
-      end
-    end
-
-    class Anything
-      def self.matches?(*)
-        true
-      end
-
-      def self.from_data(data)
-        new(data)
-      end
-
-      def initialize(data)
-        @data = data
-      end
-
-      def query
-        @data
-      end
-    end
-
     class MovieResults
       include Enumerable
 
