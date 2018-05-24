197a9d76454655a306ec7cc367237b29cf0498c6
Magnus Skog <magnus.m.skog@gmail.com>
2018-05-24 20:23:55 +0200

    Fixed duplicates in tv shows

--- app/business_logic/domain/btn/tv_show.rb
+++ app/business_logic/domain/btn/tv_show.rb
@@ -3,7 +3,10 @@ module Domain
     class TvShow < SimpleDelegator
       def self.create_from_imdb_id(imdb_id)
         search_result = Services::Search.tv_shows.search(imdb_id).first
-        new ::TvShow.create!(name: search_result.title, imdb_id: @imdb_id, tvdb_id: search_result.tvdb_id)
+        new(::TvShow.find_or_create_by(imdb_id: imdb_id) do |tv_show|
+          tv_show.name = search_result.title
+          tv_show.tvdb_id = search_result.tvdb_id
+        end)
       end
 
       def sample
