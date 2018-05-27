0911a9190d394feaaf0d35a8a4e965ab77eb40a3
Magnus Skog <magnus.m.skog@gmail.com>
2018-05-27 11:45:04 +0200

    Use new url for thumbor

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,9 +1,11 @@
 module ImageHelper
+  # TODO ENV, not raw strings
   def thumbor_image_tag(source, options={})
-    image_tag("https://thumbs.picyo.me/"+source, options)
+    image_tag("https://thumbs.mskog.com/"+source, options)
   end
 
+  # TODO ENV, not raw strings
   def thumbor_image_url(source)
-    "https://thumbs.picyo.me/#{source}"
+    "https://thumbs.mskog.com/#{source}"
   end
 end
