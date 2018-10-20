35e009039f9556787813fd48078bab8129194bb6
MSkog <magnus.m.skog@gmail.com>
2018-10-20 14:29:07 +0200

    Fixed issues with no tmdb_details

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -2,6 +2,9 @@ require "spec_helper"
 
 feature "Home page", :js, :feature do
   scenario "User visits home page" do
+    create :episode, download_at: Date.today, tv_show: create(:tv_show), tmdb_details: nil
+    create :episode, download_at: Date.yesterday, tv_show: create(:tv_show), tmdb_details: nil
+
     visit "/"
 
     expect(page).to have_text("Downloads")
