6ede66cda2a0c654e8ffe4d086cd25ef4435bbd9
Magnus Skog <magnus.m.skog@gmail.com>
2019-04-01 20:56:04 +0200

    Only watching shows

--- app/controllers/api/v1/tv_shows_calendars_controller.rb
+++ app/controllers/api/v1/tv_shows_calendars_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TvShowsCalendarsController < Api::ApiController
   def show
     @view = TvShowsCalendarDecorator.decorate ViewObjects::TvShowsCalendar.new(cache_key_prefix: 'watching')
-    render json: @view.by_date
+    render json: @view.watching.by_date
   end
 end
--- spec/requests/api/v1/tv_shows_calendar_spec.rb
+++ spec/requests/api/v1/tv_shows_calendar_spec.rb
@@ -10,20 +10,15 @@ describe "API:V1:TvShowsCalendar", type: :request do
   Given!(:credential){create :credential, name: 'trakt'}
 
   describe "Show" do
+    Given!(:tv_show_watching){create :tv_show, name: 'Teen Wolf', imdb_id: 'tt1567432', watching: true, trakt_details: {images: {poster: {thumb: 'hello.jpg'}}}}
+    Given!(:tv_show_not_watching){create :tv_show, name: 'Hannibal', imdb_id: 'some_id', watching: false}
+
     When do
       get api_v1_tv_shows_calendar_path, env: @env
     end
 
     Given(:parsed_response){JSON.parse(response.body)}
-    Given(:first_episode){parsed_response.first.second.first}
 
-    Then{expect(parsed_response.count).to eq 17}
-    And{expect(first_episode.dig("episode", "ids", "imdb")).to be_nil}
-    And{expect(first_episode.dig("episode", "ids", "trakt")).to eq 1765462}
-    And{expect(first_episode.dig("episode", "season")).to eq 5}
-    And{expect(first_episode.dig("episode", "number")).to eq 18}
-    And{expect(first_episode.dig("episode", "title")).to eq "The Maid of Gévaudan"}
-    And{expect(first_episode.dig("first_aired")).to eq "2016-02-24T02:00:00.000+00:00"}
-    And{expect(first_episode.dig("show", "title")).to eq "Teen Wolf"}
+    Then{expect(parsed_response.count).to eq 1}
   end
 end
