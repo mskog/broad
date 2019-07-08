5b73028c9c7ee0245cd1a546d56cafa3f8143b6d
Magnus Skog <magnus.m.skog@gmail.com>
2019-07-08 20:26:02 +0200

    Tv shows instead of episodes in the calendar API

--- spec/requests/api/v1/tv_shows_calendar_spec.rb
+++ spec/requests/api/v1/tv_shows_calendar_spec.rb
@@ -18,15 +18,9 @@ describe "API:V1:TvShowsCalendar", type: :request do
     end
 
     Given(:parsed_response){JSON.parse(response.body)}
-    Given(:first_episode){parsed_response.first.second.first}
+    Given(:first_show){parsed_response.first.second.first}
 
     Then{expect(parsed_response.count).to eq 1}
-    And{expect(first_episode.dig("episode", "ids", "imdb")).to be_nil}
-    And{expect(first_episode.dig("episode", "ids", "trakt")).to eq 1765462}
-    And{expect(first_episode.dig("episode", "season")).to eq 5}
-    And{expect(first_episode.dig("episode", "number")).to eq 18}
-    And{expect(first_episode.dig("episode", "title")).to eq "The Maid of Gévaudan"}
-    And{expect(first_episode.dig("first_aired")).to eq "2016-02-24T02:00:00.000+00:00"}
-    And{expect(first_episode.dig("show", "title")).to eq "Teen Wolf"}
+    And{expect(first_show["name"]).to eq tv_show_watching.name}
   end
 end
