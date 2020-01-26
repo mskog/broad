d0ac8db2411e337b78b52d690b01eb4999031f03
Magnus Skog <magnus.m.skog@gmail.com>
2020-01-26 15:06:29 +0100

    Removed the REST api. Use graphql plz

--- app/controllers/api/v1/movie_rottentomatoes_score_controller.rb
+++
@@ -1,3 +0,0 @@
-class Api::V1::MovieRottentomatoesScoreController < Api::ApiController
-  def show; end
-end
--- app/controllers/api/v1/tv_shows_calendars_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::TvShowsCalendarsController < Api::ApiController
-  def show
-    @view = TvShowsCalendarDecorator.decorate ViewObjects::TvShowsCalendar.new(cache_key_prefix: "watching")
-    render json: @view.watching.by_date
-  end
-end
