3a52c31a44f4058ed9db94c1143cb6b2a9f282ff
Magnus Skog <magnus.m.skog@gmail.com>
2020-05-10 14:46:59 +0200

    Fixes

--- app/graphql/types/movie_summary_type.rb
+++ app/graphql/types/movie_summary_type.rb
@@ -5,5 +5,7 @@ module Types
     field :overview, String, null: true
     field :rating, String, null: true
     field :status, String, null: true
+    field :runtime, Integer, null: true
+    field :released, GraphQL::Types::ISO8601Date, null: true
   end
 end
