58bbb8df5781568579307d7cac6e69722e601390
Magnus Skog <magnus.m.skog@gmail.com>
2020-07-26 11:14:11 +0200

    Pagination

--- app/graphql/resolvers/best_movies.rb
+++ app/graphql/resolvers/best_movies.rb
@@ -2,13 +2,15 @@ class Resolvers::BestMovies < Resolvers::Base
   type [Types::MovieType], null: false
 
   argument :year, type: Integer, required: false
-  argument :limit, type: Integer, required: false
+  argument :first, type: Integer, required: false
+  argument :skip, type: Integer, required: false
 
-  def resolve(year: nil, limit: 10)
+  def resolve(year: nil, first: 10, skip: nil)
     scope = Movie
             .watched
             .order("movies.personal_rating DESC NULLS LAST, movies.trakt_rating DESC")
-            .limit(limit)
+    scope = scope.offset(skip) if skip.present?
+    scope = scope.limit(first) if first.present?
     scope = scope.where("extract(year from movies.release_date) = ?", year) if year.present?
     MovieDecorator.decorate_collection ViewObjects::Movies.new(scope)
   end
