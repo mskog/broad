53936cadfedbc729e1d4c782142c79532db3400a
Magnus Skog <magnus.m.skog@gmail.com>
2020-08-08 12:23:31 +0200

    Less sleep

--- app/jobs/fetch_tv_shows_news_job.rb
+++ app/jobs/fetch_tv_shows_news_job.rb
@@ -5,7 +5,7 @@ class FetchTvShowsNewsJob < ActiveJob::Base
     ActiveRecord::Base.connection_pool.with_connection do
       TvShow.find_each do |tv_show|
         tv_show.fetch_news
-        sleep rand(30) unless Rails.env.test?
+        sleep rand(10) unless Rails.env.test?
       end
     end
   end
