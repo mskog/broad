89451899bf977525419caaa7b122e799c3d410bb
Magnus Skog <magnus.m.skog@gmail.com>
2021-04-30 20:33:41 +0200

    Top list added from PTP

---
+++ app/business_logic/services/ptp/top_movie.rb
@@ -0,0 +1,25 @@
+module Services
+  module PTP
+    class TopMovie
+      include Virtus.model
+
+      attribute :title
+      attribute :imdb_id
+      attribute :year
+      attribute :cover
+      attribute :tags, [String]
+      attribute :imdb_rating
+      attribute :mc_url
+      attribute :ptp_rating
+      attribute :youtube_id
+      attribute :synopsis
+
+      def initialize(data)
+        @data = data
+        super(Array(data).each_with_object({}) do |(key, value), new_hash|
+          new_hash[key.to_s.underscore.downcase] = value
+        end)
+      end
+    end
+  end
+end
