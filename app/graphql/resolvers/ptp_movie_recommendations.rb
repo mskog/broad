ff58b00cafc50e220d3029fc32238d343658249c
Magnus Skog <magnus.m.skog@gmail.com>
2021-05-19 19:25:04 +0200

    Caching of ptp endpoint

--- app/graphql/resolvers/ptp_movie_recommendations.rb
+++ app/graphql/resolvers/ptp_movie_recommendations.rb
@@ -2,10 +2,12 @@ class Resolvers::PTPMovieRecommendations < Resolvers::Base
   type [Types::PtpRecommendedMovieType], null: false
 
   def resolve
-    ::PTPMovieRecommendations
-      .new
-      .not_downloaded
-      .with_minimum_rating
-      .since_year(3.years.ago.year)
+    Rails.cache.fetch("graphql_ptp_movie_recommendations", expires_in: 1.day) do
+      ::PTPMovieRecommendations
+        .new
+        .not_downloaded
+        .with_minimum_rating
+        .since_year(3.years.ago.year)
+    end
   end
 end
