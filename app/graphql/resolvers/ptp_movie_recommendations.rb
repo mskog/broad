d5a4df37572af16fed5218d080e234f7ff5e02a2
Magnus Skog <magnus.m.skog@gmail.com>
2021-05-21 19:39:08 +0200

    Better caching for ptp movie recommendatiosn

--- app/graphql/resolvers/ptp_movie_recommendations.rb
+++ app/graphql/resolvers/ptp_movie_recommendations.rb
@@ -2,7 +2,8 @@ class Resolvers::PTPMovieRecommendations < Resolvers::Base
   type [Types::PtpRecommendedMovieType], null: false
 
   def resolve
-    Rails.cache.fetch("graphql_ptp_movie_recommendations", expires_in: 1.day) do
+    updated_at = Movie.maximum(:updated_at)
+    Rails.cache.fetch("graphql_ptp_movie_recommendations_#{updated_at}", expires_in: 1.day) do
       ::PTPMovieRecommendations
         .new
         .not_downloaded
