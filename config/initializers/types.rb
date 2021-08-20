a5f11804d4b7fb7744b096703e39c6a5e6964e2e
Magnus Skog <magnus.m.skog@gmail.com>
2021-08-20 18:20:52 +0200

    Fixed Top movie

--- config/initializers/types.rb
+++ config/initializers/types.rb
@@ -3,4 +3,5 @@ module Types
 
   DowncasedString = Coercible::String.constructor(&:downcase)
   UnderscoredDowncasedString = Coercible::String.constructor{|value| value.downcase.gsub(/\s+/, '_')}
+  ImdbId = Coercible::String.constructor{|value| "tt#{value}".tr("tttt", "tt")}
 end
