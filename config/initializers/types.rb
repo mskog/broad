2e5d96a7bbbc5aeeca22732eb6da5b9adf1d5f5f
Magnus Skog <magnus.m.skog@gmail.com>
2021-08-20 18:12:48 +0200

    More moved to dry

--- config/initializers/types.rb
+++ config/initializers/types.rb
@@ -1,3 +1,6 @@
 module Types
   include Dry.Types()
+
+  DowncasedString = Coercible::String.constructor(&:downcase)
+  UnderscoredDowncasedString = Coercible::String.constructor{|value| value.downcase.gsub(/\s+/, '_')}
 end
--- spec/business_logic/domain/ptp/release_rules/killer_spec.rb
+++ spec/business_logic/domain/ptp/release_rules/killer_spec.rb
@@ -5,12 +5,12 @@ describe Domain::PTP::ReleaseRules::Killer, :nodb do
 
   describe "#acceptable?" do
     context "with 3d" do
-      Given(:release){build_stubbed :movie_release, version_attributes: ["3d"]}
+      Given(:release){build_stubbed :movie_release, remaster_title: "3d"}
       Then{expect(subject).not_to be_acceptable}
     end
 
     context "with 3d half sbs" do
-      Given(:release){build_stubbed :movie_release, version_attributes: ["3d_half_sbs"]}
+      Given(:release){build_stubbed :movie_release, remaster_title: "3d_half_sbs"}
       Then{expect(subject).not_to be_acceptable}
     end
 
@@ -35,17 +35,17 @@ describe Domain::PTP::ReleaseRules::Killer, :nodb do
     end
 
     context "with a remux with a non mkv container" do
-      Given(:release){build_stubbed :movie_release, container: "m2ts", version_attributes: ["remux"]}
+      Given(:release){build_stubbed :movie_release, container: "m2ts", remaster_title: "remux"}
       Then{expect(subject).not_to be_acceptable}
     end
 
     context "with a remux with an mkv container and a bluray source" do
-      Given(:release){build_stubbed :movie_release, source: "blu-ray", container: "mkv", version_attributes: ["remux"]}
+      Given(:release){build_stubbed :movie_release, source: "blu-ray", container: "mkv", remaster_title: "remux"}
       Then{expect(subject).to be_acceptable}
     end
 
     context "with a remux with an mkv container and an hd-dvd source" do
-      Given(:release){build_stubbed :movie_release, source: "hd-dvd", container: "mkv", version_attributes: ["remux"]}
+      Given(:release){build_stubbed :movie_release, source: "hd-dvd", container: "mkv", remaster_title: "remux"}
       Then{expect(subject).to be_acceptable}
     end
   end
