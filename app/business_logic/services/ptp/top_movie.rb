5e9b1ce1534135ca3a8ed7852fe78fe4dff4df16
Magnus Skog <magnus.m.skog@gmail.com>
2021-09-04 11:15:23 +0200

    Fixed bug

--- app/business_logic/services/ptp/top_movie.rb
+++ app/business_logic/services/ptp/top_movie.rb
@@ -5,16 +5,16 @@ module Services
         key.to_s.underscore.downcase.to_sym
       end
 
-      attribute :title, Types::String.optional
-      attribute :imdb_id, Types::ImdbId.optional
-      attribute :year, Types::Coercible::Integer.optional
-      attribute :cover, Types::String.optional
-      attribute :tags, Types::Array.of(Types::String).optional
-      attribute :imdb_rating, Types::Coercible::Float.optional
-      attribute :mc_url, Types::String.optional
-      attribute :ptp_rating, Types::Coercible::Float.optional
-      attribute :youtube_id, Types::String.optional
-      attribute :synopsis, Types::String.optional
+      attribute :title?, Types::String.optional
+      attribute :imdb_id?, Types::ImdbId.optional
+      attribute :year?, Types::Coercible::Integer.optional
+      attribute :cover?, Types::String.optional
+      attribute :tags?, Types::Array.of(Types::String).optional
+      attribute :imdb_rating?, Types::Coercible::Float.optional
+      attribute :mc_url?, Types::String.optional
+      attribute :ptp_rating?, Types::Coercible::Float.optional
+      attribute :youtube_id?, Types::String.optional
+      attribute :synopsis?, Types::String.optional
     end
   end
 end
