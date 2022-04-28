ab0fd18833795d52382438b3de7aff8d9d224f7e
Magnus Skog <magnus.m.skog@gmail.com>
2022-04-28 21:08:09 +0200

    Better throttling

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,5 +1,11 @@
-Rack::Attack.throttle("episodes by ip", limit: 20, period: 3600) do |request|
-	if request.path.include?("/episodes")
+Rack::Attack.throttle("episodes index", limit: 10, period: 3600) do |request|
+	if request.path.include?("/episodes") && !request.path.include?("/download")
+		request.path
+	end
+end
+
+Rack::Attack.throttle("episodes download", limit: 2, period: 3600) do |request|
+	if request.path.include?("/episodes") && request.path.include?("download")
 		request.path
 	end
 end
