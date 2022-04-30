dce03849aa22feeadb157e76ded7079b0ff7fa54
Magnus Skog <magnus.m.skog@gmail.com>
2022-04-30 14:07:35 +0200

    No crashes when down gets nil

--- app/jobs/cache_base64_image_job.rb
+++ app/jobs/cache_base64_image_job.rb
@@ -2,11 +2,12 @@ class CacheBase64ImageJob < ActiveJob::Base
   queue_as :default
 
   def perform(url)
-    return unless url.present?
+    return if url.blank?
     Rails.cache.fetch("base64_#{url}", expires_in: 1.year) do
-      Base64.encode64(Down.download("https://thumbs.mskog.com/200x/filters:format(jpeg):quality(20):blur(10)/#{url}").read).gsub(
-        "\n", ""
-      )
+      data = Down.download("https://thumbs.mskog.com/200x/filters:format(jpeg):quality(20):blur(10)/#{url}")
+      Base64.encode64(data.read).gsub("\n", "")
+    rescue Down::Error
+      nil
     end
   end
 end
