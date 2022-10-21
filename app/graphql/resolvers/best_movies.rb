a9293d09184ab9c36fae24a4a23fd47871e9c476
Magnus Skog <magnus.skog@bredband2.se>
2022-10-21 20:50:38 +0200

    Watched Or Released

--- app/graphql/resolvers/best_movies.rb
+++ app/graphql/resolvers/best_movies.rb
@@ -1,17 +1,29 @@
+class Types::BestMovieCategory < Types::BaseEnum
+  value "RELEASED", "When the movie was released"
+  value "WATCHED", "When the movie was watched"
+end
+
 class Resolvers::BestMovies < Resolvers::Base
   type [Types::MovieType], null: false
 
-  argument :year, type: Integer, required: false
+  argument :category, Types::BestMovieCategory, required: true, default_value: "RELEASED"
+  argument :year, type: Integer, required: true
   argument :first, type: Integer, required: false
   argument :skip, type: Integer, required: false
 
-  def resolve(year: nil, first: 10, skip: nil)
+  def resolve(category: nil, year: nil, first: 10, skip: nil)
     scope = Movie
             .watched
             .order("movies.personal_rating DESC NULLS LAST, movies.trakt_rating DESC")
     scope = scope.offset(skip) if skip.present?
     scope = scope.limit(first) if first.present?
-    scope = scope.where("extract(year from movies.release_date) = ?", year) if year.present?
+
+    scope = if category == "RELEASED"
+              scope.where("extract(year from movies.release_date) = ?", year)
+            else
+              scope.where("extract(year from movies.watched_at) = ?", year)
+            end
+
     ViewObjects::Movies.new(scope)
   end
 end
--- spec/graphql/queries/best_movies_spec.rb
+++ spec/graphql/queries/best_movies_spec.rb
@@ -1,19 +1,9 @@
 require "spec_helper"
 
 describe "Best Movies", type: :request do
-  Given(:query) do
-    <<-GRAPHQL
-      {
-        bestMovies(year: 2020) {
-          id
-        }
-      }
-    GRAPHQL
-  end
-
-  Given!(:movie_2020){create :movie, release_date: "2020-01-01", watched: true}
-  Given!(:movie_2020_unwatched){create :movie, release_date: "2020-01-01"}
-  Given!(:movie_2019){create :movie, release_date: "2019-01-01", watched: true}
+  Given!(:movie_2020){create :movie, release_date: "2020-01-01", watched: true, watched_at: "2022-05-01"}
+  Given!(:movie_2020_unwatched){create :movie, release_date: "2020-01-01", watched_at: "2021-01-01"}
+  Given!(:movie_2019){create :movie, release_date: "2019-01-01", watched: true, watched_at: "2019-01-15"}
 
   When do
     post graphql_path, env: @env, params: {query: query}
@@ -21,8 +11,33 @@ describe "Best Movies", type: :request do
 
   Given(:parsed_response){JSON.parse(@response.body)}
 
-  context "Default" do
+  context "with released" do
+    Given(:query) do
+      <<-GRAPHQL
+      {
+        bestMovies(year: 2020, category: RELEASED) {
+          id
+        }
+      }
+      GRAPHQL
+    end
+
     Then{expect(parsed_response["data"]["bestMovies"].count).to eq 1}
     And{expect(parsed_response["data"]["bestMovies"].first["id"]).to eq movie_2020.id}
+  end
+
+  context "with watched" do
+    Given(:query) do
+      <<-GRAPHQL
+      {
+        bestMovies(year: 2019, category: WATCHED) {
+          id
+        }
+      }
+      GRAPHQL
+    end
+
+    Then{expect(parsed_response["data"]["bestMovies"].count).to eq 1}
+    And{expect(parsed_response["data"]["bestMovies"].first["id"]).to eq movie_2019.id}
   end
 end
